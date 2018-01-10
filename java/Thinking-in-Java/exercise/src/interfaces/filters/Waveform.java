3d4af2b1b2009f84098d38c496ed70dff8f940c7
etins <etinsj@gmail.com>
2018-01-10 11:53:52 +0800

    delete java

--- java/Thinking-in-Java/exercise/src/interfaces/filters/Waveform.java
+++
@@ -1,9 +0,0 @@
-package interfaces.filters;
-
-public class Waveform {
-    private static long counter;
-    private final long id = counter++;
-    public String toString() {
-        return "Waveform " + id;
-    }
-}
